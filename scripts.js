74b6ff468787c2041cb3e290b6a31f94b9bb007f
Lisa Carbonell <morekaleplease@gmail.com>
2019-03-13 14:55:38 -0700

    ReCode - Same problem can only click on 1st person

--- scripts.js
+++ scripts.js
@@ -1,57 +1,63 @@
-$.ajax({ //12 users from US
-    url: "https://randomuser.me/api/?results=12&nat=us",
-    dataType: "json",
-    success: function (data) {
-        console.log(data); //check to see data returned
 
-    const form = `<form action="#" method="get"> 
-                    <input type="search" id="search-input" class="search-input" placeholder="Search...">
-                    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+const searchFor = document.querySelector(".search-container"); // create search field w/ magnifying icon
+searchFor.innerHTML = `<form action="#" method-="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id+"search-submit" class="search-submit">
     </form>`;
 
-    document.querySelector('.search-container').innerHTML = form; //append search box & submit button to page
 
-    for (let i = 0; i< data.results.length; i++) { //loop thru 12 random users
+fetch('https://randomuser.me/api/?results=12&nat=us') // get 12 random users of U.S. nationality
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+        employeeCards(myJson);
+        modalWindow(myJson);
+    });
+
+    
+
+function employeeCards(myJson) { // display 12 employees in cards
+    for (let i = 0; i< myJson.results.length; i++) { //loop thru 12 random users create card gallery
         const gallery = `<div class="card">
                         <div class="card-img-container">
-                            <img class="card-img" src="${data.results[i].picture.large}" alt="profile picture">
+                            <img class="card-img" src="${myJson.results[i].picture.large}" alt="profile picture">
                         </div>
                         <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
-                        <p class="card-text">${data.results[i].email}</p>
-                        <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+                        <h3 id="name" class="card-name cap">${myJson.results[i].name.first} ${myJson.results[i].name.last}</h3>
+                        <p class="card-text">${myJson.results[i].email}</p>
+                        <p class="card-text cap">${myJson.results[i].location.city}, ${myJson.results[i].location.state}</p>
                         </div>
                     </div>`;
             
-        document.querySelector("#gallery").innerHTML += gallery;  //append each gallery card to page
-    }
+        document.querySelector("#gallery").innerHTML += gallery;
+        } 
+};
 
+function modalWindow(myJson) { // modal window that opens when employee is clicked and closes window when click on X
     $(".card").on("click", function() { // when a card is clicked show appropriate info
         let cardIndex = $('.card').index(this); // gets the index of a specific card
         const modal = `<div class="modal-container"> 
                             <div class="modal">
                                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                                 <div class="modal-info-container">
-                                    <img class="modal-img" src="${data.results[cardIndex].picture.large}" alt="profile picture">
-                                    <h3 id="name" class="modal-name cap">${data.results[cardIndex].name.first} ${data.results[cardIndex].name.last}</h3> 
-                                    <p class="modal-text">${data.results[cardIndex].email}</p>
-                                    <p class="modal-text cap">${data.results[cardIndex].location.city}</p>
+                                    <img class="modal-img" src="${myJson.results[cardIndex].picture.large}" alt="profile picture">
+                                    <h3 id="name" class="modal-name cap">${myJson.results[cardIndex].name.first} ${myJson.results[cardIndex].name.last}</h3> 
+                                    <p class="modal-text">${myJson.results[cardIndex].email}</p>
+                                    <p class="modal-text cap">${myJson.results[cardIndex].location.city}</p>
                                     <hr>
-                                    <p class="modal-text">${data.results[cardIndex].cell}</p>
-                                    <p class="modal-text cap">${data.results[cardIndex].location.street}, ${data.results[cardIndex].location.state}, ${data.results[cardIndex].nat} ${data.results[cardIndex].location.postcode}</p>
-                                    <p class="modal-text">Birthday: ${data.results[cardIndex].dob.date.slice(0, 10)}</p>
+                                    <p class="modal-text">${myJson.results[cardIndex].cell}</p>
+                                    <p class="modal-text cap">${myJson.results[cardIndex].location.street}, ${myJson.results[cardIndex].location.state}, ${myJson.results[cardIndex].nat} ${myJson.results[cardIndex].location.postcode}</p>
+                                    <p class="modal-text">Birthday: ${myJson.results[cardIndex].dob.date.slice(0, 10)}</p>
                                 </div>
                             </div> 
-                       </div>`; //close line 31 <div class="modal-container">
+                       </div>`; 
         document.querySelector("body").innerHTML += modal; //append modal to body of page
-
+    
         $('.modal-close-btn').click(function () {
             $('.modal-container').remove();
             $('.modal').remove();
-        }) //close line 48 $('.modal-close-btn').click(function()
-
-    }) // close line 29 $(".card").on("click", function()
-        
-    } //close success line 4
-}) //close $.ajax
-
+        }) 
+    });
+}
